b0788df2220dea328ac6f16ba4c2538ede948229
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-09 10:30:39 -0500

    [FIX] - % print issue

--- _printf.c
+++ _printf.c
@@ -7,39 +7,45 @@
  * @format: Format to print (int, double, char, string, ...)
  * Return: int
  */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - Prints any type of data based
+ * on format.
+ *
+ * @format: Format to print (int, double, char, string, ...)
+ * Return: int
+ */
 int _printf(const char *format, ...)
 {
 	va_list data_input;
 	unsigned int i = 0;
 	char *print_output, *buffer;
 	void *(*r)(va_list, char **buffer);
-	
-	va_start(data_input, format);
-	if (format == NULL || (format[0] == '%' && !(format[1])))
-		return (-1);
-	print_output = _calloc(1024, sizeof(char));
+
+	print_output = _calloc(512, sizeof(char));
 	if (print_output == NULL)
 		return (-1);
 	buffer = &print_output[0];
-	
-	
+	va_start(data_input, format);
+	if (format == NULL)
+		return (-1);
 	for (; format && format[i]; i++)
 	{
 		*buffer = format[i];
 		buffer++;
 		if (format[i] == '%')
 		{
-			
+			if (format[i + 1] == '\0')
+				return (-1);
 			r = match(format + i + 1);
 			if (r == NULL)
 			{
 				if (format[i + 1] == '%')
 					i++;
-				if (format[i + 1] == '\0')
-				{
-					free(print_output);
-					return(-1);
-				}
+				if (format[i + 1] == '%')
+					i++;
 				continue;
 			}
 			else
