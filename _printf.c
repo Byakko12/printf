055f0432fd6bb2cf04e2a4e38bfd2e61fbe6d4b6
DanielCeron-Holberton <3403@holbertonschool.com>
2021-11-09 08:35:02 -0500

    [FIX] - Mem conditionals and betty lines issues

--- _printf.c
+++ _printf.c
@@ -15,21 +15,19 @@ int _printf(const char *format, ...)
 	char *print_output, *buffer;
 	void *(*r)(va_list, char **buffer);
 
-	print_output = _calloc(512, sizeof(char));
+	va_start(data_input, format);
+	if (format == NULL || (format[1] == '\0' && format[0] == '%'))
+		return (-1);
+	print_output = _calloc(1024, sizeof(char));
 	if (print_output == NULL)
 		return (-1);
 	buffer = &print_output[0];
-	va_start(data_input, format);
-	if (format == NULL)
-		return (-1);
 	for (; format && format[i]; i++)
 	{
 		*buffer = format[i];
 		buffer++;
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
 			r = match(format + i + 1);
 			if (r == NULL)
 			{
